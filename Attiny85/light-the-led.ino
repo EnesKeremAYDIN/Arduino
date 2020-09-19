a3127472245c23f8dc5b0b2afaf0e116cf336aeb
EKA <46195766+EnesKeremAYDIN@users.noreply.github.com>
2020-09-19 13:56:18 +0300

    Create light-the-led.ino

---
+++ Attiny85/light-the-led.ino
@@ -0,0 +1,13 @@
+void setup() {
+// other codes
+}
+
+void loop() {
+  pinMode(1, OUTPUT); // Pin mod.
+  digitalWrite(1, HIGH); // Lights up the led. 
+  delay(500); // Wait 500 ms.
+  digitalWrite(1, LOW); // Turns off the led.
+  delay(500); // Wait 500 ms.
+}
+
+// If the light is not lit, convert the all value 1 to 0!
